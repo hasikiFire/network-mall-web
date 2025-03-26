20a150d1bdab98016fc42a31ed46d7e8d12487c1
hasikiFire <hasikifire@outlook.com>
2025-03-26 23:37:46 +0800

    fix: page export

--- src/app/manager/usageRecord/page.tsx
+++ src/app/manager/usageRecord/page.tsx
@@ -21,14 +21,14 @@ import { DataTable } from '@/components/ui/table/data-table';
 import service from './service';
 import { StatusTag } from '@/components/status-tag';
 
-export const statusStylesConfig: Record<PurchaseStatus, string> = {
+const statusStylesConfig: Record<PurchaseStatus, string> = {
   0: 'bg-blue-50 text-blue-600 border-blue-100',
   1: 'bg-green-50 text-green-600 border-green-100',
   2: 'bg-orange-50 text-orange-600 border-orange-100',
   3: 'bg-gray-50 text-gray-600 border-gray-100',
   4: 'bg-red-50 text-red-600 border-red-100'
 };
-export const columns: ColumnDef<UsageRecordListRespDto>[] = [
+const columns: ColumnDef<UsageRecordListRespDto>[] = [
   {
     accessorKey: 'id',
     header: 'ID'
